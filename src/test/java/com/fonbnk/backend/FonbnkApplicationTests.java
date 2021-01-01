42bdbde38e008e84d10641e5b870896bb892ad9b
Zubair Abubakar <zabubakar@paga.com>
2021-01-01 21:39:01 +0100

    initial setup

---
+++ src/test/java/com/fonbnk/backend/FonbnkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fonbnk.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FonbnkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
